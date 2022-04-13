f10d1fbdc6af29318406d3d1a73a6001253342cb
Anastasia <anastasiamatiek@gmail.com>
2022-04-13 21:11:44 +0300

    MPM-15-redesigned menu page

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  showFiller = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+
+
