ad8f915b53f023ba7a8d167475753380801cbbc1
Yaroslav Horievoi <yaroslavgorevoy@gmail.com>
2022-08-01 13:20:45 +0300

    MP-64

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -9,7 +9,7 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
 import { MainComponent } from './main.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ScheduleComponent } from './schedule/schedule.components';
-import {HelpAssistanceComponent} from "./help-assistance/help-assistance.component";
+import { HelpAssistanceComponent } from './help-assistance/help-assistance.component';
 
 export const mainRoutes: Routes = [
   {
